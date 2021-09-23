5a0b4a848705d69469e215fa1b897104f97ab4f7
Ilyas <yukhigov5060@mail.ru>
2021-09-23 14:14:11 +0300

    1 задания

---
+++ remember.js
@@ -0,0 +1,49 @@
+/*
+JavaScript - предназначен для написания сценариев для активных HTML-страниц.
+Язык JavaScript не имеет никакого отношения к языку Java. Java разработан фирмой SUN.
+ JavaScript - фирмой Netscape Communication Corporation.
+  Первоначальное название - LiveScript. После завоевания языком
+  Java всемирной известности LiveScript из коммерческих соображений переименовали в JavaScript.
+ */
+
+// sting, number, boolean, symbol, null, undefined, object.
+
+let emptyList = []
+
+let dataTypes = [sting, number, boolean, symbol, null, undefined, object]
+
+let dataTypesExample = ['lorem', 1, true, null, undefined, {object}]
+
+let dataTypesExample = [true, false]
+
+let collection = [
+    {
+
+    },
+    {
+
+    }
+]
+
+let application = {
+    name: 'instagramm',
+    interesting: true,
+    memory: '2.гб',
+    isItFree: true,
+    developer: {
+        name: 'facebook'
+        isItFree: true,
+        memory: '2 гб'
+    },
+}
+
+function test (a, b) {
+    if (a > d)
+        return a;
+}
+
+function startGame (playersCount) {
+    return `Игра началась. Количество игроков: ${playersCount}`
+}
+
+
